b6748af4ae3c2ab87eb218fec723428e0a6a2a5d
cloud-222 <ariadyahi@gmail.com>
2025-03-11 16:53:30 -0400

    fix(relation)

--- models/relations.js
+++ models/relations.js
@@ -1,6 +1,8 @@
 // 1. Importer les tables/modeles sans relations
 import Role from "./Role.js";
 import Employe from "./Employe.js";
+import DossierMedical from "./DossierMedical.js";
+import Consultation from "./Consultation.js";
 
 // 2. Creer les relations entre les tables/modeles
 
@@ -10,5 +12,13 @@ Role.hasMany(Employe, { foreignKey: "id_role" });
 // Un employé peut avoir un seul rôle
 Employe.belongsTo(Role, { foreignKey: "id_role" });
 
+// Relation DossierMedical - Consultation
+
+// Un dossier medical peut contenir plusieurs consultations
+DossierMedical.hasMany(Consultation, { foreignKey: "id_dossier_medical", as: "consultations" });
+
+// Une consultation est associee a un seul dossier medical
+Consultation.belongsTo(DossierMedical, { foreignKey: "id_dossier_medical", as: "dossierMedical" });
+
 // 3. Exporter les tables/modeles avec relations vers les controllers
-export { Role, Employe };
+export { Role, Employe, DossierMedical, Consultation };
