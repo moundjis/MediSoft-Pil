e6e77b652f448548a714953616543287e4644d5d
Moundji Sahraoui <2726205@collegelacite.ca>
2025-03-16 12:04:09 -0400

    creation du frontEnd avec react et nextjs

--- client/app/page.js
+++ client/app/page.js
@@ -1,81 +1,103 @@
-"use client";
-
-import Employee from "@/components/employee/Employee";
-import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  const [employees, setEmployees] = useState([]); // Liste des employés
-  const [detail, setDetail] = useState(); // Détails de l'employé sélectionné
-  const [error, setError] = useState(null); // Gestion des erreurs
-  const [loading, setLoading] = useState(false); // Gestion de l'état de chargement
-
-  useEffect(() => {
-    GetAllEmployees();
-  }, []);
-
-  //============Functions===============//
-
-  async function GetAllEmployees() {
-    setLoading(true);
-    try {
-      const response = await fetch("http://localhost:5000/api/employe");
-      if (!response.ok) {
-        throw new Error("Failed to fetch employees");
-      }
-      const employees = await response.json();
-      setEmployees(employees.data);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  function AfficheSelection(detail) {
-    setDetail(detail);
-  }
-
-  async function DeleteEmployee(id) {
-    try {
-      const response = await fetch(`http://localhost:5000/api/employe/${id}`, {
-        method: "DELETE",
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to delete employee");
-      }
-
-      setEmployees((prevEmployees) => prevEmployees.filter((e) => e.id !== id));
-    } catch (error) {
-      setError(error.message);
-    }
-  }
-
-  //======================================//
-
   return (
-    <div className="">
-      <h2>Lists of employees</h2>
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      <ul>
-        {employees.map((employee) => (
-          <li key={employee.id}>
-            <Employee
-              employee={employee}
-              afficherSelection={AfficheSelection}
-              deleteEmployee={DeleteEmployee}
-            />
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              app/page.js
+            </code>
+            .
           </li>
-        ))}
-      </ul>
-      <h2>Chosen one</h2>
-      {detail && (
-        <div>
-          <span>{detail}</span>
-          <button onClick={() => setDetail(null)}>Reset</button>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      )}
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
