19c47cd81673d18a1cfc6e1a43502d8636dc2d73
cloud-222 <ariadyahi@gmail.com>
2025-03-17 13:48:22 -0400

    feat(prescription,rendezvous,patients):ajout de linterface pour chaque model

---
+++ client/app/patients/page.jsx
@@ -0,0 +1,5 @@
+import Patients from "@/components/Patients/Patients";
+
+export default function Home() {
+  return <Patients />;
+}
---
+++ client/components/Prescription/Prescription.jsx
@@ -0,0 +1,28 @@
+import prescriptions from "@/public/data/prescriptions";
+
+export default function Prescriptions() {
+  return (
+    <div className="h-full bg-white rounded-2xl drop-shadow-[5px_5px_3px_rgba(0,0,0,0.5)] p-15 mr-5 mt-10 mb-10">
+      <h1 className="font-bold text-2xl text-black py-2">
+        Liste des prescriptions
+      </h1>
+      <button className="text-white text-sm bg-blue-400 px-2 mb-2 hover:cursor-pointer hover:bg-blue-600">
+        Ajouter
+      </button>
+      <table className="w-full">
+        <thead>
+          <tr className="bg-yellow-400">
+            {prescriptions.map((prescriptions) => (
+              <th
+                key={prescriptions.id}
+                className="px-4 py-3 text-left text-sm font-medium text-white border border-3"
+              >
+                {prescriptions.name}
+              </th>
+            ))}
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
